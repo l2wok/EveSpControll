7ae6cb0759e538e44c60e88d746110fa6cd5c06a
l2wok <l22wok@gmail.com>
2017-06-28 22:34:25 +0300

    Add new code
    character choose in console

---
+++ Test.py
@@ -0,0 +1,46 @@
+import sys, eveapi
+
+YOUR_KEYID = 6312835
+YOUR_VCODE = '5K2SsxqUY7D97pemhH89FWOVei2splAr3f4xzAhd7ExZQXXBLYT6U6ekLZVTJnHp'
+
+
+def choosen(characters):
+    if not characters:
+        print('You api is broken, characters not found.')
+    else:
+        start = 0
+        for chars in characters:
+            start+=1
+            print(start, chars.name)
+
+        takehime = input('Кого использовать (номер)?\n')
+        if takehime.isdigit():
+            char = int(takehime)
+            if char in [1,2,3]:
+                if char <= len(characters):
+                    return char-1
+
+        print("Oшибка выбора, вы выбрали '",takehime,"'")
+        choosen(characters)
+
+
+def main(args):
+    eveapi.set_user_agent("eveapi.py/1.3")
+    api = eveapi.EVEAPIConnection()
+    auth = api.auth(keyID=YOUR_KEYID, vCode=YOUR_VCODE)
+    result = auth.account.Characters()
+
+    charnum = choosen(result.characters)
+
+    ret = auth.character(result.characters[charnum].characterID)	
+    sheet = ret.CharacterSheet()
+
+    print(sheet.name)
+    #skilltree = api.eve.SkillTree()
+    #print()
+
+    #for g in skilltree.skillGroups:
+
+
+if __name__ == '__main__':
+    main(sys.argv)
